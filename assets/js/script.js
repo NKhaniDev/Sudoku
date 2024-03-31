9973f58f8a11384bf4b012626e6e230fd2d71d36
NKaniDev <khani.navid65@gmail.com>
2024-03-31 16:55:11 +0200

    fixing bugs in modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
-//modal section for how to play
+//modal section for how to play- w3 school
 document.addEventListener("DOMContentLoaded", function(){
-let modal=document.getElementById("my-modal");
-let myButton=document.getElementById("my-button");
-let span= document.getElementsByClassName("close");
+   let modal = document.getElementById("my-modal");
+   let myButton = document.getElementById("my-button");
+   let span = document.getElementsByClassName("close")[0];
 
-}
+    myButton.onclick= function(){
+       modal.style.display="block";
+    };
+
+    span.onclick=function(){
+       modal.style.display="none";
+    };
+
+    windows.onclick= function(event){
+      if(event.target== modal){
+          modal.style.display="none";
+       }
+    };
+
+});
 
 
