114dc539f1e7483fa3e24d321c2a283f2701ec07
NKaniDev <khani.navid65@gmail.com>
2024-03-31 16:02:38 +0200

    script for modal section(how to play)

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//modal section for how to play
+document.addEventListener("DOMContentLoaded", function(){
+let modal=document.getElementById("my-modal");
+let myButton=document.getElementById("my-button");
+let span= document.getElementsByClassName("close");
+
+}
+
+
