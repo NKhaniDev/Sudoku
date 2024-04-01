0a78f4a6d63cf9dfcd6ae68be9c848c57fdc9fc0
NKaniDev <khani.navid65@gmail.com>
2024-04-01 11:12:54 +0200

    function to load the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 });
 
-// generating 3 span elemnt including life class
+// populating 3 span elemnt including life class
 
 let LivesContainer=document.getElementById("lives");
 for(let j=0; j<3;j++){
@@ -29,10 +29,12 @@ for(let j=0; j<3;j++){
     LivesContainer.appendChild(span);
 
 }
+
 // initial numbers and the solution for puzzle
 var board = '6------7------5-2------1---362----81--96-----71--9-4-5-2---651---78----345-------';
 var solution ='685329174971485326234761859362574981549618732718293465823946517197852643456137298';
-// function to chnge the series of numbers to the array of rows
+
+// function to change the series of numbers to the array of rows
 
 function convertToSudokuRows(puzzleString){
     let size= 9;
@@ -45,5 +47,11 @@ function convertToSudokuRows(puzzleString){
 
 var board = convertToSudokuRows(board)
 var solution = convertToSudokuRows(solution)
+
+// starting the game-credit to: https://www.youtube.com/watch?v=S4uRtTb8U-U&t=648s
+
+window.onload = function(){
+    setGame();
+}
 
 
